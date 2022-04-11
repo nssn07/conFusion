835e501451f0e9cacf1f2c61c52aac3f70f989c5
nssn07 <nssn07.2020@gmail.com>
2022-04-12 04:48:44 +0530

    logo correction

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,5 +9,5 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
     { path: 'home' , component: HomeComponent },
     { path: 'menu' , component: MenuComponent },
-    { path: '' , redirectTo: '/home' ,pathMatch: 'full' }
+    { path: '' , redirectTo: 'home' ,pathMatch: 'full' }
 ];
