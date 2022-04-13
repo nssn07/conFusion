00a332e2a8a754b35d782f93a155e13fa29d20e8
nssn07 <nssn07.2020@gmail.com>
2022-04-13 17:47:02 +0530

    Single Page Applications Part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
@@ -39,6 +40,7 @@ import { ContactComponent } from './contact/contact.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatListModule,
+    MatIconModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
